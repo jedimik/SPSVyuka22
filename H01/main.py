d52d2c81694ed2ca832dc07006d8978c42da4937
jedimik_laptop <>
2022-10-11 08:10:47 +0200

    ..

---
+++ H01/main.py
@@ -0,0 +1,101 @@
+
+
+
+if __name__=="__main__":
+
+    # a="5"
+    # if a>=5:
+    #     print("A je vetsi nez 5")
+    # elif a>=5:
+    #     print("A je vetsi rovno 5")
+    # else:
+    #     print("A je nejake")
+    # if a==5:
+    #     print("A je 5")
+    # if a>=5:
+    #     print("Ahoj Svete")
+    
+
+    # a=3
+    # if a>3:
+    #     a+=2
+    # else:
+    #     a+=1
+    # if a>10/2:
+    #     a*=5
+    # else:
+    #     a-=4
+    # print(a)
+
+
+    # a=6
+
+    # if a==5:
+    #     print("A je == 5")
+    # elif a==4:
+    #     print("A je == 4")
+    # elif a==3:
+    #     print("A je rovno 3")
+    # elif a==2:
+    #     print("A je rovno 2")
+    # elif a>=5:
+    #     print("A je vetsi rovno 5")
+    # elif a==6:
+    #     print("A je rovno 6")
+    # else:
+    #     print("A neni 5")
+    # #Jakmile plati jedna podminka z bloku, jsem venku
+    # if a>=4:
+    #     print('A je vetsi nez 4 pisu to z druheho if bloku')
+
+    # b=4
+    # c=4
+    # if b>4:
+    #     print("Be je vetsi nez 4")
+    #     if b==5:
+    #         print("B je rovno 5")
+    #     else:
+    #         print("B neni rovno 5")
+    # elif b==4:
+    #     if c>5:
+    #         print("C je vetsi nez 5")
+    #     else:
+    #         print("C je mensi nez 5")
+    # else:
+    #     print('Hello world')
+
+    # Cykly
+    # b=0
+    # while b<5:
+    #     print(b)
+    #     b+=1
+    #     print(b)
+        # print(b+1)
+
+    # a=0
+    # for i in range(0,5,1):
+    #     for j in range(0,4,1):
+    #         for x in range(0,3,1):
+    #             a+=1
+    #             break
+    # a+=1
+    # if a%4 ==1:
+    #     a-=4                        
+    # print(a,end="")
+
+    a=0
+    for i in range(0,5,1):
+        for j in range(0,5,1):
+            if j%2==0:
+                print("*",end="")
+            else:
+                print("/",end="")
+        print()
+    # print(a)
+    ###########################################3
+    # a=0
+    # for i in range(0,5,1):
+    #     for j in range(0,5,1):
+    #         a+=1
+    # print(a)
+
